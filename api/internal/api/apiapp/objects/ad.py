c3b2a938db72289a0ab259766dfd5119076a4547
waawal <waawal@boom.ws>
2012-06-07 14:05:22 +0200

    changed datetime to timestamp to avoid namespace conflicts

--- api/internal/api/apiapp/objects/ad.py
+++ api/internal/api/apiapp/objects/ad.py
@@ -1,6 +1,6 @@
 """ Returns a mapping representing the objects. """
 
-def Ad(user=None, text=None, active=False, datetime=None, coords=None,
+def Ad(user=None, text=None, active=False, timestamp=None, coords=None,
        region=None, media=None, storage=None):
     """ Returns a mapping representing a ad. """
     pass
