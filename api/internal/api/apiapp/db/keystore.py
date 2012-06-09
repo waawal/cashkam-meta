166ca4e4b53755f24809c618f98410628f1d53de
waawal <waawal@boom.ws>
2012-06-09 21:57:04 +0200

    implemented keystore for auth

--- api/internal/api/apiapp/db/keystore.py
+++ api/internal/api/apiapp/db/keystore.py
@@ -8,10 +8,11 @@ import redis
 
 
 AUTH_PREFIX = "auth"
+USERS_PREFIX = "users"
 
 con = redis.ConnectionPool()
 
-def auth_hashed(username, password):
+def auth_hash(username, password):
     """ Calculates the hash and returns it """
     token = ":".join((username, password))
     return hashlib.sha512(token).hexdigest()
@@ -22,13 +23,22 @@ def get_auth(username, password):
     """
     authtoken = auth_hash(username, password)
     result = con.get(":".join((AUTH_PREFIX, authtoken)))
-    if result:
-        return result
-    else:
-        # Raise
+    return result
 
 def post_auth(username, password):
-    pass
+    """ Creates a hash token and sets a rediskey.
+        A user is also created with a reference back to the hash.
+    """
+    authtoken = auth_hash(username, password)
+    if con.setnx(":".join((USERS_PREFIX, username)),
+                 ":".join((AUTH_PREFIX, authtoken))):
+        result = con.setnx(":".join((AUTH_PREFIX, authtoken)), username)
+        return result
+    return False
 
 def put_auth(name, password, newPassword):
-    pass
+    authtoken = auth_hash(name, password)
+    newauthtoken = auth_hash(name, newPassword)
+    result = con.renamenx(":".join((AUTH_PREFIX, authtoken)),
+                          ":".join((AUTH_PREFIX, newauthtoken)))
+    return result
