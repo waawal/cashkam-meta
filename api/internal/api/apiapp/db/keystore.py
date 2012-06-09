ae5ddc63d1767345a523f4176e7ff625b8a7b058
waawal <waawal@boom.ws>
2012-06-09 12:55:54 +0200

    implemented auth hashing in sha512

--- api/internal/api/apiapp/db/keystore.py
+++ api/internal/api/apiapp/db/keystore.py
@@ -1,13 +1,31 @@
 """ The keystore backend, built with Redis """
 
+
+import hashlib
 import json
 
 import redis
 
+
+AUTH_PREFIX = "auth"
+
 con = redis.ConnectionPool()
 
+def auth_hashed(username, password):
+    """ Calculates the hash and returns it """
+    token = ":".join((username, password))
+    return hashlib.sha512(token).hexdigest()
+
 def get_auth(username, password):
-    pass
+    """ Tries to get the authentication token, if it fail, it raises a
+        HTTPError 401.
+    """
+    authtoken = auth_hash(username, password)
+    result = con.get(":".join((AUTH_PREFIX, authtoken)))
+    if result:
+        return result
+    else:
+        # Raise
 
 def post_auth(username, password):
     pass
