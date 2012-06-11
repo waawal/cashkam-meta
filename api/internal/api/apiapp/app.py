9e51dab12a2c0e0267a4092775af2a0dffabb7dd
waawal <waawal@boom.ws>
2012-06-11 14:27:51 +0200

    implemented http routing

--- api/internal/api/apiapp/app.py
+++ api/internal/api/apiapp/app.py
@@ -2,11 +2,38 @@
 
 import json
 
-from bottle import (get,
-                    post,
-                    put,
-                    delete,
-                    request,
-                    )
+import bottle
+
 
 import db
+
+import ad
+import ads
+
+import auth
+
+import user
+import users
+
+def setup_routing(app):
+    """ Routing. """
+    app.route('/ad', method=['GET'], ad.get_ad)
+    app.route('/ad', method=['PUT'], ad.put_ad)
+    app.route('/ad', method=['DELETE'], ad.del_ad)
+    
+    app.route('/ads', method=['GET'], ad.get_ads)
+    app.route('/ads', method=['POST'], ad.post_ads)
+    
+    app.route('/auth', method=['GET'], auth.get_auth)
+    app.route('/auth', method=['POST'], auth.post_auth)
+    app.route('/auth', method=['PUT'], auth.put_auth)
+    
+    app.route('/user', method=['GET'], user.get_user)
+    app.route('/user', method=['PUT'], user.put_user)
+    
+    app.route('/user', method=['DELETE'], user.del_user)
+    app.route('/users', method=['GET'], users.get_users)
+    app.route('/users', method=['POST'], users.get_users)
+
+app = Bottle()
+setup_routing(app)
