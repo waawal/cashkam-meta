abc57d0f5b8544eae4de15cac6392a3cfc94f030
waawal <waawal@boom.ws>
2012-06-11 14:38:44 +0200

    fixing kwargs

--- api/internal/api/apiapp/app.py
+++ api/internal/api/apiapp/app.py
@@ -17,23 +17,23 @@ import users
 
 def setup_routing(app):
     """ Routing. """
-    app.route('/ad', method=['GET'], ad.get_ad)
-    app.route('/ad', method=['PUT'], ad.put_ad)
-    app.route('/ad', method=['DELETE'], ad.del_ad)
+    app.route('/ad', method=['GET'], callback=ad.get_ad)
+    app.route('/ad', method=['PUT'], callback=ad.put_ad)
+    app.route('/ad', method=['DELETE'], callback=ad.del_ad)
     
-    app.route('/ads', method=['GET'], ad.get_ads)
-    app.route('/ads', method=['POST'], ad.post_ads)
+    app.route('/ads', method=['GET'], callback=ad.get_ads)
+    app.route('/ads', method=['POST'], callback=ad.post_ads)
     
-    app.route('/auth', method=['GET'], auth.get_auth)
-    app.route('/auth', method=['POST'], auth.post_auth)
-    app.route('/auth', method=['PUT'], auth.put_auth)
+    app.route('/auth', method=['GET'], callback=auth.get_auth)
+    app.route('/auth', method=['POST'], callback=auth.post_auth)
+    app.route('/auth', method=['PUT'], callback=auth.put_auth)
     
-    app.route('/user', method=['GET'], user.get_user)
-    app.route('/user', method=['PUT'], user.put_user)
+    app.route('/user', method=['GET'], callback=user.get_user)
+    app.route('/user', method=['PUT'], callback=user.put_user)
     
-    app.route('/user', method=['DELETE'], user.del_user)
-    app.route('/users', method=['GET'], users.get_users)
-    app.route('/users', method=['POST'], users.get_users)
+    app.route('/user', method=['DELETE'], callback=user.del_user)
+    app.route('/users', method=['GET'], callback=users.get_users)
+    app.route('/users', method=['POST'], callback=users.get_users)
 
 app = Bottle()
 setup_routing(app)
