e781c6d6418ab24b7ac540454085def6666a67d2
waawal <waawal@boom.ws>
2012-06-11 14:52:34 +0200

    forwarding paths correctly to api-methods

--- api/internal/api/apiapp/app.py
+++ api/internal/api/apiapp/app.py
@@ -14,23 +14,23 @@ import user
 import users
 
 
-def setup_routing(apiapp):
+def setup_routing(app):
     """ Routing. """
-    app.route('/ad', method=['GET'], callback=ad.get_ad)
-    app.route('/ad', method=['PUT'], callback=ad.put_ad)
-    app.route('/ad', method=['DELETE'], callback=ad.del_ad)
+    app.route('/ad/:id', method=['GET'], callback=ad.get_ad)
+    app.route('/ad/:id', method=['PUT'], callback=ad.put_ad)
+    app.route('/ad/:id', method=['DELETE'], callback=ad.del_ad)
     
     app.route('/ads', method=['GET'], callback=ads.get_ads)
     app.route('/ads', method=['POST'], callback=ads.post_ads)
     
     app.route('/auth', method=['GET'], callback=auth.get_auth)
     app.route('/auth', method=['POST'], callback=auth.post_auth)
-    app.route('/auth', method=['PUT'], callback=auth.put_auth)
+    app.route('/auth/:name', method=['PUT'], callback=auth.put_auth)
     
-    app.route('/user', method=['GET'], callback=user.get_user)
-    app.route('/user', method=['PUT'], callback=user.put_user)
+    app.route('/user/:name', method=['GET'], callback=user.get_user)
+    app.route('/user/:name', method=['PUT'], callback=user.put_user)
+    app.route('/user/:name', method=['DELETE'], callback=user.del_user)
     
-    app.route('/user', method=['DELETE'], callback=user.del_user)
     app.route('/users', method=['GET'], callback=users.get_users)
     app.route('/users', method=['POST'], callback=users.get_users)
 
