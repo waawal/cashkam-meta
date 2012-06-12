f803e3e58da75d8699e1ad05c80bf248ee2a4b58
waawal <waawal@boom.ws>
2012-06-12 16:09:23 +0200

    implemented put_user

--- api/internal/api/apiapp/user.py
+++ api/internal/api/apiapp/user.py
@@ -9,13 +9,20 @@ def get_user(name):
     validate_queries('name')
     try:
         return db.doc_store.user.one({'name':name}).to_json()
-    except:
+    except: # TODO: Bug! one raises exception if > 1
         raise HTTPError(404)
 
 @requires_auth
 def put_user(name, authed):
-    validate_queries(db.doc_store.User.structure)
-    pass
+    r = validate_queries(db.doc_store.User.structure)
+    if name == authed:
+        user = db.doc_store.user.one({'name':name})
+        if not user:
+            raise HTTPError404
+        user.update(r)
+        return user.save()
+    
+    raise HTTPError(403)
 
 @requires_auth
 def del_user(name, authed):
