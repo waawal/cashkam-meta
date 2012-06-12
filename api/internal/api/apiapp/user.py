53c29ef4f9bd41f1496b2172b38cf7119e29197d
waawal <waawal@boom.ws>
2012-06-12 11:58:04 +0200

    get_user now raises 404 if user not found

--- api/internal/api/apiapp/user.py
+++ api/internal/api/apiapp/user.py
@@ -7,7 +7,10 @@ from utils import requires_auth, validate_queries
 
 def get_user(name):
     validate_queries('name')
-    return db.doc_store.user.one({'name':name}).to_json()
+    try:
+        return db.doc_store.user.one({'name':name}).to_json()
+    except:
+        raise HTTPError(404)
 
 @requires_auth
 def put_user(name, authed):
